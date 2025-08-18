ceee557326a96dbb71d55e513ec8444bb508759a
Brandon Kyle Dela Cruz <brandon.k.dela.cruz@accenture.com>
2025-08-18 06:48:51 +0000

    initial commit

---
+++ webapp/controller/g5_CaseStudy.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("casestudy.controller.g5_CaseStudy", {
+        onInit() {
+        }
+    });
+});
