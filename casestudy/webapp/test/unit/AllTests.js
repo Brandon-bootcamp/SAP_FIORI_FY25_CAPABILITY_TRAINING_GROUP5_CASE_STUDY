2141ab3fe48f2c1d7d98d970268052190d3d2fba
Arjake Vrench Corpuz <arjake.vrench.corpuz@accenture.com>
2025-08-20 10:10:56 +0000

    redundant files removed

--- casestudy/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"casestudy/test/unit/controller/detail.controller"
-], function () {
-	"use strict";
-});
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"casestudy/test/unit/controller/g5_CaseStudy.controller"
-], function () {
-	"use strict";
-});
