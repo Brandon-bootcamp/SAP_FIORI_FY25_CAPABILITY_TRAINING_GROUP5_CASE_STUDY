511e2e483e1a7c5168246e37be0ef8f506bac5d7
Stanlee John Lantano <stanlee.john.lantano@accenture.com>
2025-08-26 05:51:43 +0000

    minor changes

--- webapp/controller/DetailPage.controller.js
+++ webapp/controller/DetailPage.controller.js
@@ -6,7 +6,21 @@ sap.ui.define([
     return Controller.extend("com.ordermanagement.ordermanagement.controller.DetailPage", {
         
 onInit: function () {
-      },
+      const oRouter = this.getOwnerComponent().getRouter();
+  oRouter.getRoute("RouteDetailPage").attachPatternMatched(this._onRouteMatched, this);
+},
+
+_onRouteMatched(oEvent) {
+  const orderId = oEvent.getParameter("arguments").orderId;
+  const oModel = this.getView().getModel();
+ 
+  // Assuming /Orders is your collection
+  const selectedOrder = oModel.getProperty("/Orders").find(order => order.OrderNumber === orderId);
+ 
+  if (selectedOrder) {
+    this.getView().setModel(new sap.ui.model.json.JSONModel(selectedOrder), "selectedOrder");
+  }
+},
 
 onEdit: function () {
       var oRouter = this.getOwnerComponent().getRouter();
