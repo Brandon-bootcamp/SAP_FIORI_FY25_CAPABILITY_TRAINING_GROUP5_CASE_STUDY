17057b6f61627a18680c49fc4ace31f386622bbe
Stanlee John Lantano <stanlee.john.lantano@accenture.com>
2025-08-26 05:35:42 +0000

    minor changes

--- webapp/controller/DetailPage.controller.js
+++ webapp/controller/DetailPage.controller.js
@@ -6,15 +6,6 @@ sap.ui.define([
     return Controller.extend("com.ordermanagement.ordermanagement.controller.DetailPage", {
         
 onInit: function () {
-//   var oModel = new sap.ui.model.json.JSONModel({
-//     orderNumber: "012201",
-//     createdOn: "01 Jan 2025",
-//     receivingPlant: "9101 - Singapore",
-//     deliveringPlant: "9102 - Malaysia",
-//     status: "Created",
-//     products: ""
-//   });
-//   this.getView().setModel(oModel, "orderModel");
       },
 
 onEdit: function () {
