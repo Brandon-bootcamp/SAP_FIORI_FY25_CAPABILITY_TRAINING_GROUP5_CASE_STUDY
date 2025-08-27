f30a02c5e611d281af5cf72b4d8aaa6c4171c35f
Dela Cruz <brandon.k.dela.cruz@accenture.com>
2025-08-28 02:36:32 +0800

    detail page done

--- webapp/controller/DetailPage.controller.js
+++ webapp/controller/DetailPage.controller.js
@@ -1,31 +1,53 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, JSONModel, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("com.ordermanagement.ordermanagement.controller.DetailPage", {
-        
-onInit: function () {
-//   var oModel = new sap.ui.model.json.JSONModel({
-//     orderNumber: "012201",
-//     createdOn: "01 Jan 2025",
-//     receivingPlant: "9101 - Singapore",
-//     deliveringPlant: "9102 - Malaysia",
-//     status: "Created",
-//     products: ""
-//   });
-//   this.getView().setModel(oModel, "orderModel");
-      },
 
-onEdit: function () {
-      var oRouter = this.getOwnerComponent().getRouter();
-      oRouter.navTo("RouteEditPage");
-    },
+        onInit: function () {
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.getRoute("RouteDetailPage").attachPatternMatched(this._onObjectMatched, this);
+        },
 
-onCancel: function () {
-            var oRouter = this.getOwnerComponent().getRouter();
+        _onObjectMatched: function (oEvent) {
+            const sOrderNumber = oEvent.getParameter("arguments").orderId;
+            const oModel = this.getView().getModel();
+
+            // Read the specific Order and expand Products
+            oModel.read(`/Orders(${sOrderNumber})`, {
+                urlParameters: {
+                    "$expand": "Products"
+                },
+                success: (oData) => {
+                    const oLocalModel = new JSONModel({
+                        Order: oData,
+                        Products: oData.Products || []
+                    });
+                    this.getView().setModel(oLocalModel, "localOrders");
+                    console.log(oLocalModel);
+                },
+                error: (err) => {
+                    console.error("Failed to fetch order details:", err);
+                }
+            });
+        },
+
+        onEdit: function (oEvent) {
+            const selectedOrder = oEvent.getSource().getBindingContext("localOrders").getObject();
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("RouteEditPage", {
+                orderId: selectedOrder.OrderNumber
+            });
+        },
+
+        onCancel: function () {
+            const oRouter = this.getOwnerComponent().getRouter();
             oRouter.navTo("RouteOrderMainPage");
-      }  
-      
+        }
+
     });
 });
